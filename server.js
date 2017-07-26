22d93e97e07f3ddfafef4d2cd91951cf4b3629d7
phytertek <ryan.phytertek@gmail.com>
2017-07-26 13:21:14 -0400

    Server.js finished

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+app.get('/multiply/:x/:y', (req, res) => {
+    res.send(req.params.x * req.params.y)
+})
+app.get('/books', (req, res) => {
+    const books = ['Congo', 'Atlas Shrugged', 'The Phoenix Project']
+    if (req.query.index) res.send(books[req.query.index])
+    else res.send(books)
+})
+app.listen(3000, () => { console.log('Server running on port 3000') })
